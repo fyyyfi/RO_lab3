b2b8bcc94ded741d8b9e84cfc491b19233a1f301
Kateryna <katyuha.mik@gmail.com>
2025-11-02 15:42:37 +0000

    Gauss functions

--- lab3/ParallelGauss.cpp
+++ lab3/ParallelGauss.cpp
@@ -171,3 +171,119 @@ void PrintResultVector (double* pResult, int Size) {
     for (i=0; i<Size; i++)
         printf("%7.4f ", pResult[pParallelPivotPos[i]]);
 }
+
+// Fuction for the column elimination
+void ParallelEliminateColumns(double* pProcRows, double* pProcVector,
+double* pPivotRow, int Size, int RowNum, int Iter) {
+    double multiplier;
+
+    for (int i=0; i<RowNum; i++) {
+        if (pProcPivotIter[i] == -1) {
+            multiplier = pProcRows[i*Size+Iter] / pPivotRow[Iter];
+            for (int j=Iter; j<Size; j++) {
+                pProcRows[i*Size + j] -= pPivotRow[j]*multiplier;
+            }
+            pProcVector[i] -= pPivotRow[Size]*multiplier;
+        }
+    }
+}
+
+// Function for the Gaussian elimination
+void ParallelGaussianElimination (double* pProcRows, double* pProcVector,
+int Size, int RowNum) {
+    double MaxValue; // Value of the pivot element of thе process
+    int PivotPos; // Position of the pivot row in the process stripe
+
+    // Structure for the pivot row selection
+    struct { double MaxValue; int ProcRank; } ProcPivot, Pivot;
+
+    // pPivotRow is used for storing the pivot row and the corresponding
+    // element of the vector b
+    double *pPivotRow; // Pivot row of the current iteration
+    pPivotRow = new double [Size+1];
+
+    // The iterations of the Gaussian elimination stage
+    for (int i=0; i<Size; i++) {
+        // Calculating the local pivot row
+        double MaxValue = 0;
+
+        for (int j=0; j<RowNum; j++) {
+            if ((pProcPivotIter[j] == -1) &&
+            (MaxValue < fabs(pProcRows[j*Size+i]))) {
+                MaxValue = fabs(pProcRows[j*Size+i]);
+                PivotPos = j;
+            }
+        }
+
+        ProcPivot.MaxValue = MaxValue;
+        ProcPivot.ProcRank = ProcRank;
+
+        // Finding the pivot process
+        // (process with the maximum value of MaxValue)
+        MPI_Allreduce(&ProcPivot, &Pivot, 1, MPI_DOUBLE_INT, MPI_MAXLOC, MPI_COMM_WORLD);
+
+        // Broadcasting the pivot row
+        if ( ProcRank == Pivot.ProcRank ){
+            pProcPivotIter[PivotPos]= i; //iteration number
+            pParallelPivotPos[i]= pProcInd[ProcRank] + PivotPos;
+        }
+
+        MPI_Bcast(&pParallelPivotPos[i], 1, MPI_INT, Pivot.ProcRank, MPI_COMM_WORLD);
+
+        if ( ProcRank == Pivot.ProcRank ) {
+            // Fill the pivot row
+            for (int j=0; j<Size; j++) {
+                pPivotRow[j] = pProcRows[PivotPos*Size + j];
+            }
+            pPivotRow[Size] = pProcVector[PivotPos];
+        }
+
+        MPI_Bcast(pPivotRow, Size+1, MPI_DOUBLE, Pivot.ProcRank, MPI_COMM_WORLD);
+        ParallelEliminateColumns(pProcRows, pProcVector, pPivotRow, Size, RowNum, i);
+    }
+}
+
+// Function for finding the pivot row of the back substitution
+void FindBackPivotRow(int RowIndex, int &IterProcRank,
+int &IterPivotPos) {
+    for (int i=0; i<ProcNum-1; i++) {
+        if ((pProcInd[i]<=RowIndex) && (RowIndex<pProcInd[i+1]))
+            IterProcRank = i;
+    }
+
+    if (RowIndex >= pProcInd[ProcNum-1])
+        IterProcRank = ProcNum-1;
+    IterPivotPos = RowIndex - pProcInd[IterProcRank];
+}
+
+// Function for the back substitution
+void ParallelBackSubstitution (double* pProcRows, double* pProcVector,
+double* pProcResult, int Size, int RowNum) {
+    int IterProcRank; // Rank of the process with the current pivot row
+    int IterPivotPos; // Position of the pivot row of the process
+    double IterResult; // Calculated value of the current unknown
+    double val;
+
+    // Iterations of the back substitution stage
+    for (int i=Size-1; i>=0; i--) {
+        // Calculating the rank of the process, which holds the pivot row
+        FindBackPivotRow (pParallelPivotPos[i], IterProcRank, IterPivotPos);
+
+        // Calculating the unknown
+        if (ProcRank == IterProcRank) {
+            IterResult =
+            pProcVector[IterPivotPos]/pProcRows[IterPivotPos*Size+i];
+            pProcResult[IterPivotPos] = IterResult;
+        }
+
+        // Broadcasting the value of the current unknown
+        MPI_Bcast(&IterResult, 1, MPI_DOUBLE, IterProcRank, MPI_COMM_WORLD);
+
+        // Updating the values of the vector b
+        for (int j=0; j<RowNum; j++)
+            if ( pProcPivotIter[j] < i ) {
+                val = pProcRows[j*Size + i] * IterResult;
+            pProcVector[j]=pProcVector[j] - val;
+        }
+    }
+}
