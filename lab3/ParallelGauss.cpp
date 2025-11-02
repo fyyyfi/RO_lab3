f218de2b176e17f6e5f75633826e7f30932b19e1
Kateryna <katyuha.mik@gmail.com>
2025-11-02 14:24:40 +0000

    base functions

--- lab3/ParallelGauss.cpp
+++ lab3/ParallelGauss.cpp
@@ -91,3 +91,83 @@ double* &pProcResult, int &Size, int &RowNum) {
         // RandomDataInitialization(pMatrix, pVector, Size);
     }
 }
+// Function for the data distribution among the processes
+void DataDistribution(double* pMatrix, double* pProcRows, double* pVector,
+double* pProcVector, int Size, int RowNum) {
+    int *pSendNum; // Number of the elements sent to the process
+    int *pSendInd; // Index of the first data element sent to the process
+    int RestRows=Size; // Number of rows, that have not been distributed yet
+    int i; // Loop variable
+
+    // Alloc memory for temporary objects
+    pSendInd = new int [ProcNum];
+    pSendNum = new int [ProcNum];
+
+    // Define the disposition of the matrix rows for the current process
+    RowNum = (Size/ProcNum);
+    pSendNum[0] = RowNum*Size;
+    pSendInd[0] = 0;
+
+    for (i=1; i<ProcNum; i++) {
+        RestRows -= RowNum;
+        RowNum = RestRows/(ProcNum-i);
+        pSendNum[i] = RowNum*Size;
+        pSendInd[i] = pSendInd[i-1]+pSendNum[i-1];
+    }
+
+    // Scatter the rows
+    MPI_Scatterv(pMatrix, pSendNum, pSendInd, MPI_DOUBLE, pProcRows,
+    pSendNum[ProcRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    // Define the disposition of the matrix rows for current process
+    RestRows = Size;
+    pProcInd[0] = 0;
+    pProcNum[0] = Size/ProcNum;
+
+    for (i=1; i<ProcNum; i++) {
+        RestRows -= pProcNum[i-1];
+        pProcNum[i] = RestRows/(ProcNum-i);
+        pProcInd[i] = pProcInd[i-1]+pProcNum[i-1];
+    }
+
+    MPI_Scatterv(pVector, pProcNum, pProcInd, MPI_DOUBLE, pProcVector,
+    pProcNum[ProcRank], MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+    // Free the memory
+    delete [] pSendNum;
+    delete [] pSendInd;
+}
+
+// Function for gathering the result vector
+void ResultCollection(double* pProcResult, double* pResult) {
+    // Gather the whole result vector on every processor
+    MPI_Gatherv(pProcResult, pProcNum[ProcRank], MPI_DOUBLE, pResult,
+    pProcNum, pProcInd, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+}
+
+// Function for formatted matrix output
+void PrintMatrix (double* pMatrix, int RowCount, int ColCount) {
+    int i, j; // Loop variables
+
+    for (i=0; i<RowCount; i++) {
+        for (j=0; j<ColCount; j++)
+            printf("%7.4f ", pMatrix[i*ColCount+j]);
+        printf("\n");
+    }
+}
+
+// Function for formatted vector output
+void PrintVector (double* pVector, int Size) {
+    int i;
+
+    for (i=0; i<Size; i++)
+        printf("%7.4f ", pVector[i]);
+}
+
+// Function for formatted vector output
+void PrintResultVector (double* pResult, int Size) {
+    int i;
+    
+    for (i=0; i<Size; i++)
+        printf("%7.4f ", pResult[pParallelPivotPos[i]]);
+}
